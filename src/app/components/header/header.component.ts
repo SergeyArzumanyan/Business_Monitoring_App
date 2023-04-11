1beb94a8ed3707425c5c5f4680d6aeb6f79dafb7
Sergey Arzumanyan <sergey.arzumanyan@am.technamin.com>
2023-04-12 00:17:03 +0400

    TS-1 => fixed some ui issues

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,11 +6,11 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  public isMobile = (window.innerWidth <= 850);
+  public isMobile = (window.innerWidth <= 700);
 
   @HostListener("window:resize", ["$event.target"])
   private onWindowResize(): void {
-    this.isMobile = (window.innerWidth <= 850);
+    this.isMobile = (window.innerWidth <= 700);
   }
 
 }
