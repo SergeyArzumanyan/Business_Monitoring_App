1e4d18538ec9bda21513e083a97e16aacc7a310f
Sergey Arzumanyan <Sergey.Arzumanyan.work@gmail.com>
2023-08-30 23:51:03 +0400

    Pushing For Environment Change.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,25 +1,25 @@
-// export const environment =  {
-//   production: "false",
-//   firebase: {
-//     apiKey: 'AIzaSyBCJvhzW8729wh2wxmkD1bOszykJGmrfV0',
-//     appId: '1:609910362494:web:3261241469cd583512e32f',
-//     authDomain: 'truffle-sweets.firebaseapp.com',
-//     databaseURL: 'https://truffle-sweets-default-rtdb.europe-west1.firebasedatabase.app',
-//     messagingSenderId: '609910362494',
-//     projectId: 'truffle-sweets',
-//     storageBucket: 'truffle-sweets.appspot.com',
-//   },
-// };
-
 export const environment =  {
-  production: "true",
+  production: "false",
   firebase: {
-    apiKey: "AIzaSyDN4Hd60u8S9gN0DKOtJDsXLiq1nsYUrgs",
-    authDomain: "truffle-sweets-prod.firebaseapp.com",
-    databaseURL: "https://truffle-sweets-prod-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "truffle-sweets-prod",
-    storageBucket: "truffle-sweets-prod.appspot.com",
-    messagingSenderId: "1055829966507",
-    appId: "1:1055829966507:web:fa77c0f697659c3c7701ee"
-  }
+    apiKey: 'AIzaSyBCJvhzW8729wh2wxmkD1bOszykJGmrfV0',
+    appId: '1:609910362494:web:3261241469cd583512e32f',
+    authDomain: 'truffle-sweets.firebaseapp.com',
+    databaseURL: 'https://truffle-sweets-default-rtdb.europe-west1.firebasedatabase.app',
+    messagingSenderId: '609910362494',
+    projectId: 'truffle-sweets',
+    storageBucket: 'truffle-sweets.appspot.com',
+  },
 };
+
+// export const environment =  {
+//   production: "true",
+//   firebase: {
+//     apiKey: "AIzaSyDN4Hd60u8S9gN0DKOtJDsXLiq1nsYUrgs",
+//     authDomain: "truffle-sweets-prod.firebaseapp.com",
+//     databaseURL: "https://truffle-sweets-prod-default-rtdb.europe-west1.firebasedatabase.app",
+//     projectId: "truffle-sweets-prod",
+//     storageBucket: "truffle-sweets-prod.appspot.com",
+//     messagingSenderId: "1055829966507",
+//     appId: "1:1055829966507:web:fa77c0f697659c3c7701ee"
+//   }
+// };
